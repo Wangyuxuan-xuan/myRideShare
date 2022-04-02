cf00054fa644faa0a7dfc45f6ee87eb67248eded
Wangyuxuan-xuan <wangyx0316@126.com>
2022-04-02 16:47:02 +0200

    Styled cards section

--- frontend/src/components/cards/Cards.tsx
+++ frontend/src/components/cards/Cards.tsx
@@ -13,30 +13,30 @@ function Cards() {
                         <ul className="cards__items">
                             <CardItems
                                 src = "../../../images/img-1.jpg"
-                                text = "this picture size is changeable by the length of text"
+                                text = "No matter where you’re going, by carpool, find the perfect ride from our wide range of destinations and routes at low prices."
                                 label = "Adventure"
                                 path = "/search"/>
                             <CardItems
                                 src = "../../../images/img-2.jpg"
-                                text = "this picture size is changeable by the length of text"
-                                label = "Label 2"
+                                text = "Booking a ride has never been easier! Thanks to our simple app powered by great technology, you can book a ride close to you in just minutes."
+                                label = "Leisure"
                                 path = "/search"/>
                         </ul>
                         <ul className="cards__items">
                             <CardItems
                                 src = "../../../images/img-3.jpg"
-                                text = "this picture size is changeable by the length of text"
-                                label = "Label 3"
+                                text = "We take the time to get to know each of our members and bus partners. We check reviews, profiles and IDs, so you know who you’re travelling with and can book your ride at ease on our secure platform."
+                                label = "Business"
                                 path = "/search"/>
                             <CardItems
                                 src = "../../../images/img-4.jpg"
-                                text = "this picture size is changeable by the length of text"
-                                label = "Label 4"
+                                text = "Every week, every month. To meet with a loved one, or to discover a new place. With a big family, or a big luggage. To Paris, Amsterdam or any other European destination."
+                                label = "Travelling"
                                 path = "/search"/>
                             <CardItems
                                 src = "../../../images/img-5.jpg"
-                                text = "this picture size is changeable by the length of text"
-                                label = "Label 5"
+                                text = "Carpooling is one of the world's best community-based travel network. No matter where you're going, by bus or carpool, find the perfect ride from our wide range of destinations and routes at low prices."
+                                label = "Community"
                                 path = "/search"/>
                         </ul>
                     </div>
